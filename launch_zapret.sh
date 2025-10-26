430290625ad19a82a671d816f2d9fa140038b005
h1tpo1nt <90309559+h1tpo1nt@users.noreply.github.com>
2025-10-27 02:57:58 +0300

    Update launch_zapret.sh

--- launch_zapret.sh
+++ launch_zapret.sh
@@ -2,6 +2,10 @@
 ZAPRET_DIR="/Users/h1tpo1nt/Downloads/zapret/zapret-v72.2"
 LISTS="$ZAPRET_DIR/lists"
 BIN="$ZAPRET_DIR/bin"
+
+# Задайте GameFilter, если не задана
+GameFilter=${GameFilter:-"2053,2083,2087,2096,8443"}
+
 cd "$ZAPRET_DIR"
 
 ./binaries/mac64/tpws \
