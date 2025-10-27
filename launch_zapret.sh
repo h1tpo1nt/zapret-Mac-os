4d3a8d15d58f242d0d0ae71fa470956a94f523f7
h1tpo1nt <90309559+h1tpo1nt@users.noreply.github.com>
2025-10-27 03:30:23 +0300

    Update launch_zapret.sh

--- launch_zapret.sh
+++ launch_zapret.sh
@@ -2,7 +2,7 @@
 ZAPRET_DIR="/Users/h1tpo1nt/Downloads/zapret/zapret-v72.2"
 LISTS="$ZAPRET_DIR/lists"
 cd "$ZAPRET_DIR"
-./binaries/mac64/tpws --port=988 --daemon --pidfile=/tmp/zapret.pid --hostlist="$LISTS/list-general.txt" --split-pos=2 --disorder
+sudo ./binaries/mac64/tpws --port=988 --daemon --pidfile=/tmp/zapret.pid --hostlist="$LISTS/list-general.txt" --split-pos=2 --disorder
 echo "zapret tpws запущен на порту 988"
 sleep 1
 if [ -f /tmp/zapret.pid ]; then
