6d27a67ab3360eb4d905a20afa5e910697102c57
h1tpo1nt <90309559+h1tpo1nt@users.noreply.github.com>
2025-10-27 04:01:02 +0300

    Create launch_zapret_socks_.sh

---
+++ launch_zapret_socks_.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ZAPRET_DIR="/Users/h1tpo1nt/Downloads/zapret/zapret-v72.2"
+LISTS="$ZAPRET_DIR/lists"
+BIN="$ZAPRET_DIR/bin"
+
+cd "$ZAPRET_DIR"
+
+# Ваша переменная GameFilter (установите нужные порты, если есть)
+GameFilter="2053,2083,2087,2096,8443"
+
+# Запуск tpws в режиме SOCKS c наборами стратегий по аналогии с Windows-скриптом 
+# Разбито на отдельные вызовы с --new
+
+sudo ./binaries/mac64/tpws --port=1988 --socks --daemon --pidfile=/tmp/zapret_socks.pid --wf-tcp="80,443,2053,2083,2087,2096,8443,$GameFilter" --wf-udp="443,19294-19344,50000-50100,$GameFilter" --new
+
+sudo ./binaries/mac64/tpws --socks --hostlist="$LISTS/list-general.txt" --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="$BIN/quic_initial_www_google_com.bin" --new
+
+sudo ./binaries/mac64/tpws --socks --filter-udp="19294-19344,50000-50100" --filter-l7="discord,stun" --dpi-desync=fake --dpi-desync-repeats=6 --new
+
+sudo ./binaries/mac64/tpws --socks --filter-tcp=80 --hostlist="$LISTS/list-general.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=2 --new
+
+sudo ./binaries/mac64/tpws --socks --filter-tcp="2053,2083,2087,2096,8443" --hostlist-domains="discord.media" --dpi-desync=fake --dpi-desync-fake-tls-mod=none --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=2 --new
+
+sudo ./binaries/mac64/tpws --socks --filter-tcp=443 --hostlist="$LISTS/list-general.txt" --dpi-desync=fake --dpi-desync-fake-tls-mod=none --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=2 --new
+
+sudo ./binaries/mac64/tpws --socks --filter-udp=443 --ipset="$LISTS/ipset-all.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="$BIN/quic_initial_www_google_com.bin" --new
+
+sudo ./binaries/mac64/tpws --socks --filter-tcp=80 --ipset="$LISTS/ipset-all.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=2 --new
+
+sudo ./binaries/mac64/tpws --socks --filter-tcp="443,$GameFilter" --ipset="$LISTS/ipset-all.txt" --dpi-desync=syndata --new
+
+sudo ./binaries/mac64/tpws --socks --filter-udp="$GameFilter" --ipset="$LISTS/ipset-all.txt" --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=12 --dpi-desync-any-protocol=1 --dpi-desync-fake-unknown-udp="$BIN/quic_initial_www_google_com.bin" --dpi-desync-cutoff=n2
+
+echo "zapret tpws (SOCKS) запущен на порту 1988"
+echo "Настройте системный SOCKS5 прокси на 127.0.0.1:1988"
