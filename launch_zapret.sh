a66caaa8da05b48efe200528aee5222efcf78d8b
h1tpo1nt <90309559+h1tpo1nt@users.noreply.github.com>
2025-10-27 03:04:43 +0300

    Update launch_zapret.sh

--- launch_zapret.sh
+++ launch_zapret.sh
@@ -2,23 +2,20 @@
 
 ZAPRET_DIR="/Users/h1tpo1nt/Downloads/zapret/zapret-v72.2"
 LISTS="$ZAPRET_DIR/lists"
-BIN="$ZAPRET_DIR/bin"
-
-# При необходимости задайте GameFilter переменную, например:
-GameFilter="2053,2083,2087,2096,8443"
 
 cd "$ZAPRET_DIR"
 
+# Запуск tpws с обходом DPI для доменов из list-general.txt
 ./binaries/mac64/tpws \
---filter-tcp=80,443,2053,2083,2087,2096,8443 \
---filter-udp=443,19294-19344,50000-50100,$GameFilter \
---filter-tcp=80 --hostlist="$LISTS/list-general.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=2 --new \
---filter-tcp=443 --hostlist="$LISTS/list-general.txt" --dpi-desync=fake --dpi-desync-fake-tls-mod=none --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=2 --new \
---filter-udp=443 --hostlist="$LISTS/list-general.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="$BIN/quic_initial_www_google_com.bin" --new \
---filter-tcp=80 --ipset="$LISTS/ipset-all.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=10000000 --new \
---filter-tcp=443 --ipset="$LISTS/ipset-all.txt" --dpi-desync=fake --dpi-desync-fake-tls-mod=none --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=10000000 --new \
---filter-udp=443 --ipset="$LISTS/ipset-all.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="$BIN/quic_initial_www_google_com.bin" --new \
---filter-tcp=2053,2083,2087,2096,8443 --hostlist-domains=discord.media --dpi-desync=fake --dpi-desync-fake-tls-mod=none --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=2 --new \
---filter-udp=19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-repeats=6 --new \
---filter-tcp=443,$GameFilter --ipset="$LISTS/ipset-all.txt" --dpi-desync=fake --dpi-desync-fake-tls-mod=none --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=10000000 --new \
---filter-udp=$GameFilter --ipset="$LISTS/ipset-all.txt" --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=10 --dpi-desync-any-protocol=1 --dpi-desync-fake-unknown-udp="$BIN/quic_initial_www_google_com.bin" --dpi-desync-cutoff=n2
+--port=988 \
+--daemon \
+--pidfile=/tmp/zapret.pid \
+--hostlist="$LISTS/list-general.txt" \
+--split-pos=2 \
+--disorder \
+--oob \
+--hostcase \
+--domcase
+
+echo "zapret tpws запущен на порту 988"
+echo "PID сохранён в /tmp/zapret.pid"
